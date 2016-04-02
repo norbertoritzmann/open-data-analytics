5085c3402a88600e659617e590f525fccb2e6794
Norberto Ritzmann Júnior <norberto.ritzmann@gmail.com>
2016-04-01 21:32:08 -0300

    Adding the project machine-learning

---
+++ open-data-analytics/src/main/java/com/dsciclub/open_data_analytics/App.java
@@ -0,0 +1,13 @@
+package com.dsciclub.open_data_analytics;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
